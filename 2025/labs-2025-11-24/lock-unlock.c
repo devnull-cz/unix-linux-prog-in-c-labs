87804986c3e94f404f021ded1469e599eff77f2b
Vladimir Kotal <vlada@kotalovi.cz>
2025-11-24 19:38:07 +0100

    add solution

--- 2025/labs-2025-11-24/lock-unlock.c
+++ 2025/labs-2025-11-24/lock-unlock.c
@@ -1,61 +1,65 @@
 /*
- * A simple program to provide file locking.  It has 2 parameters, 1st is the
- * lock filename, 2nd one specifies whether you lock or unlock.  It can NOT
- * recover from the situation when somebody leaves the file lock behind (and
- * exits, for example).  I.e. it does not know how to figure out whether the
- * lock is "stale".
+ * Demonstrate how to use named pipe to avoid busy waiting when lock file
+ * already exists.
  *
- * It might be tempting to tackle the problem of stale locks by writing the PID
- * of the process owning the lock to the file, then check whether the process is
- * still alive. If not, you write new PID to the lock file.  However, the
- * operation of reading the PID, checking its existence, and writing the new
- * lock file is again NOT atomic so this can lead to incorrect situations.
+ * Based on lock-unlock.c.
  *
- * See run.sh in this directory as an example script that uses this program.
- *
- * The app that uses this simple locking mechanism must busy wait.  That is
- * usually not acceptable in the real world.
- *
- * If successful, the program returns 0 if the lock/unlock operation succeeded,
- * 1 if it cannot lock since it is already locked, and 2 if either of those
- * operations failed unexpectedly.
- *
- * See ./run.sh for more information.
- *
- * (c) jp@devnull.cz
+ * (c) jp@devnull.cz, vlada@kotalovi.cz
  */
 
+#include <stdio.h>
 #include <err.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <string.h>
-#include <sys/types.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-#define	OPL	"lock"
-#define	OPUNL	"unlock"
+#define	PIPE_PATH	"/tmp/lock_pipe"
 
 int
 lock(char *filename)
 {
-	do {
-		errno = 0;
-		if (open(filename, O_RDONLY | O_TRUNC | O_EXCL | O_CREAT, 0666) != -1)
-			return (0);
-
-	} while (errno != EEXIST);
+again:
+	int pipefd;
+	if (open(filename,
+	    O_RDONLY | O_TRUNC | O_EXCL | O_CREAT, 0666) != -1) {
+		printf("Acquired the lock\n");
+		/*
+		 * Have to open as read-write. If O_WRONLY was used,
+		 * it would wait for the reader to open, blocking itself.
+		 */
+		if ((pipefd = open(PIPE_PATH, O_RDWR)) == -1)
+			err(1, "pipe open write");
+		return (pipefd);
+	}
 
 	if (errno != EEXIST)
 		err(2, "open");
 
-	return (1);
+	/* Wait for the unlock. */
+	printf("Waiting for unlock\n");
+	if ((pipefd = open(PIPE_PATH, O_RDONLY)) == -1)
+		err(1, "pipe open read-only");
+	char buf[1];
+	read(pipefd, buf, sizeof (buf));
+	close(pipefd);	// no longer needed
+	goto again;
+
+	// Not reached.
+	return (-1);
 }
 
 int
-unlock(char *filename)
+unlock(char *filename, int pipefd)
 {
+	printf("Unlocking\n");
 	if (unlink(filename) == -1)
 		err(2, "unlink");
+
+	if (pipefd != -1)
+		close(pipefd);
 
 	return (0);
 }
@@ -63,14 +67,15 @@ unlock(char *filename)
 int
 main(int argc, char **argv)
 {
-	if (argc != 3)
-		errx(1, "usage: %s <lockfile> %s | %s", argv[0], OPL, OPUNL);
+	if (argc != 2)
+		errx(1, "usage: %s <lockfile>", argv[0]);
 
-	if (strcmp(argv[2], OPL) != 0 && strcmp(argv[2], OPUNL) != 0)
-		errx(1, "usage: %s <lockfile> %s | %s", argv[0], OPL, OPUNL);
+	// TODO: should check whether this is a pipe
+	if (mkfifo(PIPE_PATH, 0666) == -1 && errno != EEXIST)
+		err(1, "mkfifo");
 
-	if (strcmp(argv[2], OPL) == 0)
-		return (lock(argv[1]));
-
-	return (unlock(argv[1]));
+	int pipefd = lock(argv[1]);
+	printf("Sleeping for a bit\n");
+	sleep(10);
+	unlock(argv[1], pipefd);
 }
